91923057beb357b3c1875999afa3c13633bdec9b
ziadalsafe <zalsafe4@gmail.com>
2023-01-30 14:41:05 +0300

    create repo done

--- lib/featuers/home/data/repos/home_repository_impl.dart
+++ lib/featuers/home/data/repos/home_repository_impl.dart
@@ -1,23 +1,18 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
-
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
-
 import 'package:book_app/core/error/feilds.dart';
 import 'package:book_app/featuers/home/data/model/book_model/book_model.dart';
-
 import '../../../../core/utils/api_services.dart';
 import 'home_repository.dart';
 
 class RepoHomeImpl implements Repo {
   Api api;
-  List<BookModel> dataModel;
+  List<BookModel>? dataModel;
 
   RepoHomeImpl(
     this.api,
-    this.dataModel,
   );
 
   @override
@@ -28,14 +23,14 @@ class RepoHomeImpl implements Repo {
           endPoint:
               '?q=subject:Programming&Filtering=free-ebooks&Projection=full&Download=epub');
       for (var doc in data['item']) {
-        dataModel.add(doc);
+        dataModel!.add(doc);
       }
     } catch (e) {
       if (e is DioError) {
         left(ServerFeiluer.fromErrorDio(e));
       }
     }
-    return right(dataModel);
+    return right(dataModel!);
   }
 
   @override
@@ -45,13 +40,13 @@ class RepoHomeImpl implements Repo {
           endPoint:
               '?q=subject:Programming&Filtering=free-ebooks&Download=epub');
       for (var doc in data['item']) {
-        dataModel.add(doc);
+        dataModel!.add(doc);
       }
     } catch (e) {
       if (e is DioError) {
         left(ServerFeiluer.fromErrorDio(e));
       }
     }
-    return right(dataModel);
+    return right(dataModel!);
   }
 }
